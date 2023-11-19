72c7d5e05f644071b33145506fbc1ee790a32eb9
Tiuiu.SikachovaEA@outlook.com <Tiuiu.SikachovaEA@outlook.com>
2023-11-19 20:05:45 +0500

    Добавьте файлы проекта.

---
+++ Tiuiu.SikachevaEA.Sprint5.Task0.V9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tiuiu.SikachevaEA.Sprint5.Task0.V9.Lib;
+
+namespace Tiuiu.SikachevaEA.Sprint5.Task0.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
