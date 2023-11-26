3b83502995cac6d3c75676b487a01b624d0ff972
SikahevaEA <144147492+SikahevaEA@users.noreply.github.com>
2023-11-26 23:54:39 +0500

    Add files via upload

---
+++ Tiuiu.SikachevaEA.Sprint5.Task7.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tiuiu.SikachevaEA.Sprint5.Task7.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tiuiu.SikachevaEA.Sprint5.Task7.V30.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b25428c5-62e7-4d9f-be83-3da56db05185")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tiuiu.SikachevaEA.Sprint5.Task7.V30/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tiuiu.SikachevaEA.Sprint5.Task7.V30.Lib;
+
+namespace Tiuiu.SikachevaEA.Sprint5.Task7.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V30.txt";
+            string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Находится в файле: ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
